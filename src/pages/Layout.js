424e4d16ecee92b6676f634fd589a94c2e692697
Ignas Veleckis <ignas.veleckis@tesonet.com>
2021-03-15 09:11:38 +0200

    fixed deploy

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./Landing";
 import SudokuCreateForm from "./SudokuCreateForm";
 
 const Layout = () => {
   return (
-    <Router basename="/">
+    <Router>
       <Switch>
         <Route path="/" exact>
           <Landing />
